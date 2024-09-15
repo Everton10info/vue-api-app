b82b18289dd6149a6c236293bd3b4bd574563db7
everton_bellmann <everton.bellmann@klab.com.br>
2024-09-15 17:48:39 -0300

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,49 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Deus no comando</h1>
+    <h1>Dados da API</h1>
+    <div v-for="item in items" :key="item.id" class="item">
+      <h2>{{ item.title }}</h2>
+      <img :src="'https://image.tmdb.org/t/p/w200'+item.poster_path" />
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from 'axios';
+var access_token = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhYTExYjg5ZWU5NDczZWI3OGI5YjI2OWZjMjRiMzYzMiIsInN1YiI6IjYxODVjYzY0MDc1Mjg4MDAyODdlYTVmYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.0bICTq0SqShQGdpbeyZGflikn9mAe0-h2BeE35I9RYU';
+var url ="https://api.themoviedb.org/3/search/multi?query=$popular&include_adult=true&language=en-US&page=5"
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      items: [],
+    };
+  },
+  created() {
+    this.fetchItems();
+  },
+  methods: {
+    async fetchItems() {
+      try {
+        const response = await axios.get(url, {
+          headers: {
+            'Authorization': `Bearer ${access_token}`
+          }
+        }); // Substitua pela URL da sua API
+        this.items = response.data['results'];
+      } catch (error) {
+        console.error('Erro ao buscar itens:', error);
+      }
+    },
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+img {
+  width: 600px;
+  /* Ajuste o tamanho conforme necessário */
+  height: auto;
+  margin: 10px;
 }
 </style>
